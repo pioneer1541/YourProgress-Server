ef93e7398651a8dcbc5faff3d83c4c694fa31a30
Vincent <pioneer1541@gmail.com>
2021-09-22 23:00:33 +1200

    make it better

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://progress-list.web.app/");
 
   // Request methods you wish to allow
   res.setHeader(
