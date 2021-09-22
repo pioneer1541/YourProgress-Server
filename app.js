b2614a0129cace96b1a1f2003083fe0fe4ccdc74
Vincent <pioneer1541@gmail.com>
2021-09-22 23:04:18 +1200

    deal with Acess Control

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "https://progress-list.web.app/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
